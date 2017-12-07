4cb39bdf6f2a57aab703f59eb5cf16c09e3a4a1e
masato-ka <nsb21708@nifty.com>
2017-12-07 22:32:40 +0900

    Heroku向けに自活スクリプトを設定

--- src/main/java/ka/masato/twitter/twitterwordcloud/CrawlerTask.java
+++ src/main/java/ka/masato/twitter/twitterwordcloud/CrawlerTask.java
@@ -1,6 +1,7 @@
 package ka.masato.twitter.twitterwordcloud;
 
 import ka.masato.twitter.twitterwordcloud.domain.wordcount.service.WordCountsService;
+import ka.masato.twitter.twitterwordcloud.infra.HerokuClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,18 +11,25 @@ import twitter4j.TwitterException;
 public class CrawlerTask {
 
     private final WordCountsService wordCountsService;
+    private final HerokuClient herokuClient;
 
     @Value("${crawling.repeat.interval:600000}")
     private int crawlingTime;
 
-    public CrawlerTask(WordCountsService wordCountsService) {
+    public CrawlerTask(WordCountsService wordCountsService, HerokuClient herokuClient) {
         this.wordCountsService = wordCountsService;
+        this.herokuClient = herokuClient;
     }
 
     @Scheduled(fixedDelayString = "${crawling.repeat.interval:600000}")
     private void doCrawlingTwitter() throws TwitterException {
         int timeSpan = crawlingTime/60000;
         wordCountsService.indexingWordCount("有馬記念",100, timeSpan);
+    }
+
+    @Scheduled(cron = "* */20 * * * *", zone = "Asia/Tokyo")
+    private void doKickSelf() {
+        this.herokuClient.getHerokuOwn();
     }
 
 }
---
+++ src/main/java/ka/masato/twitter/twitterwordcloud/infra/HerokuClient.java
@@ -0,0 +1,34 @@
+package ka.masato.twitter.twitterwordcloud.infra;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+@Slf4j
+@Component
+public class HerokuClient {
+
+    private RestTemplate restTemplate;
+    @Value("${heroku.own.url}")
+    private String getUrl;
+
+    public HerokuClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public void getHerokuOwn() {
+        try {
+            URI url = new URI(getUrl);
+            restTemplate.getForObject(url, String.class);
+        } catch (URISyntaxException e) {
+            log.error("Failed setting URL for heroku.own.url");
+        }
+    }
+}
