2d623df9c0cbc81d4fcc06d2b4c700ac607b5a43
masato-ka <nsb21708@nifty.com>
2017-12-13 22:52:42 +0900

    集計クエリを修正 時刻指定ありのクエリでGROUP BY句が抜けていたため追加。

--- src/main/java/ka/masato/twitter/twitterwordcloud/domain/wordcount/repository/WordCountSummaryRepository.java
+++ src/main/java/ka/masato/twitter/twitterwordcloud/domain/wordcount/repository/WordCountSummaryRepository.java
@@ -13,12 +13,12 @@ import java.util.List;
 public interface WordCountSummaryRepository extends JpaRepository<WordCountSummary,Integer>{
 
     @Query(value = "SELECT NEW ka.masato.twitter.twitterwordcloud.domain.wordcount.model.WordCountSummary(w.word.wordId,w.word,SUM(w.count))" +
-            "FROM WordCounts w WHERE w.word = :word AND w.time BETWEEN :start AND :end")
-    WordCountSummary sumCountByWordAndPeriod(@Param("word") String word, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+            "FROM WordCounts w WHERE w.word = :word AND w.time BETWEEN :startTime AND :endTime GROUP BY w.word")
+    WordCountSummary sumCountByWordAndPeriod(@Param("word") String word, @Param("startTime") LocalDateTime start, @Param("endTime") LocalDateTime end);
 
     @Query(value = "SELECT NEW ka.masato.twitter.twitterwordcloud.domain.wordcount.model.WordCountSummary(w.word.wordId,w.word,SUM(w.count))" +
-            "FROM WordCounts w WHERE w.time BETWEEN :start AND :end")
-    List<WordCountSummary> sumCountByPeriod(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+            "FROM WordCounts w WHERE w.time BETWEEN :startTime AND :endTime GROUP BY w.word")
+    List<WordCountSummary> sumCountByPeriod(@Param("startTime") LocalDateTime start, @Param("endTime") LocalDateTime end);
 
     @Query(value = "SELECT NEW ka.masato.twitter.twitterwordcloud.domain.wordcount.model.WordCountSummary(w.word.wordId,w.word,SUM(w.count)) " +
             "FROM WordCounts w WHERE w.word.word = :word GROUP BY w.word")
